f653f5dabec386fd40c1c7147650c57e29c7fef0
杜斌斌 <dubinbin@ciyun.cn>
2019-06-23 23:40:04 +0800

    Initial commit

---
+++ Algorithm/SwapAB.h
@@ -0,0 +1,25 @@
+//
+//  SwapAB.h
+//  Algorithm
+//
+//  Created by CYKJ on 2019/5/24.
+
+
+#ifndef SwapAB_h
+#define SwapAB_h
+
+#import <stdio.h>
+/**
+  *  @brief   交换两个数字
+  */
+void swapAB(int a, int b)
+{
+    a = a + b;
+    b = a - 2 b;   // a - b
+    a = (a - b) / 2; // (a - a + 2 b)/2
+    b = a + b;
+    
+    printf
+}
+
+#endif /* SwapAB_h */
